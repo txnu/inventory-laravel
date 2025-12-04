9e32d275aace1f994a3f45dc5744231b4a9a9cdb
TANU WIJAYA <92568646+txnu@users.noreply.github.com>
2025-12-05 04:13:07 +0700

    update customer migration

--- database/migrations/2025_12_03_161625_create_customers_table.php
+++ database/migrations/2025_12_03_161625_create_customers_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email');
             $table->bigInteger('phone');
             $table->string('address');
-            $table->boolean('is_active');
+            $table->boolean('status');
             $table->timestamps();
         });
     }
