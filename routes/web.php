b26c446b0153147ccb3d1ca503a5ac9f9ea64060
TANU WIJAYA <92568646+txnu@users.noreply.github.com>
2026-04-19 03:51:11 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ use Milon\Barcode\DNS1D;
 Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
+
 // Product Route
 Route::get('/product', [ProductController::class, 'index'])->name('product.index');
 Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
@@ -26,6 +27,7 @@ Route::post('/product/store', [ProductController::class, 'store'])->name('produc
 Route::get('/product/{id}', [ProductController::class, 'detail'])->name('product.detail');
 Route::put('/product/update/{id}', [ProductController::class, 'update'])->name('product.update');
 Route::delete('/product/delete/{id}', [ProductController::class, 'delete'])->name('product.delete');
+
 
 // Category Route
 Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
@@ -35,17 +37,21 @@ Route::get('/category/{id}', [CategoryController::class, 'detail'])->name('categ
 Route::put('/category/update/{id}', [CategoryController::class, 'update'])->name('category.update');
 Route::delete('/category/delete/{id}', [CategoryController::class, 'delete'])->name('category.delete');
 
+
 //Stock Adjustments Route
 Route::get('/stock-adjustment', [StockAdjustmentController::class, 'index'])->name('stock-adjustment.index');
 Route::get('/stock-adjustment/create', [StockAdjustmentController::class, 'create'])->name('stock-adjustment.create');
 Route::post('/stock-adjustment/store', [StockAdjustmentController::class, 'store'])->name('stock-adjustment.store');
 
+
 //Stock Movements Route
 Route::get('/stock-movement', [StockMovementController::class, 'index'])->name('stock-movement.index');
+
 
 // Stock Route
 Route::get('/stock', [StockController::class, 'index'])->name('stock.index');
 Route::get('/stock/create', [StockController::class, 'create'])->name('stock.create');
+
 
 // Warehouse Route
 Route::get('/warehouse', [WarehouseController::class, 'index'])->name('warehouse.index');
@@ -57,8 +63,6 @@ Route::put('/warehouse/update/{id}', [WarehouseController::class, 'update'])->na
 Route::delete('/warehouse/delete/{id}', [WarehouseController::class, 'destroy'])->name('warehouse.delete');
 
 
-// Transactions Route
-
 // Purchase Order
 Route::get('/purchase-order', [PurchaseOrderController::class, 'index'])->name('purchase-order.index');
 Route::get('/purchase-order/create', [PurchaseOrderController::class, 'create'])->name('purchase-order.create');
@@ -67,6 +71,7 @@ Route::get('/purchase-order/show/{id}', [PurchaseOrderController::class, 'show']
 Route::get('/purchase-order/edit/{id}', [PurchaseOrderController::class, 'edit'])->name('purchase-order.edit');
 Route::put('/purchase-order/update/{id}', [PurchaseOrderController::class, 'update'])->name('purchase-order.update');
 Route::delete('/purchase-order/delete/{id}', [PurchaseOrderController::class, 'destroy'])->name('purchase-order.delete');
+
 
 // Purchase Receiving
 Route::get('/purchase-receiving', [PurchaseReceivingController::class, 'index'])->name('purchase-receiving.index');
@@ -89,6 +94,7 @@ Route::get('/supplier/{id}', [SupplierController::class, 'edit'])->name('supplie
 Route::put('/supplier/update/{id}', [SupplierController::class, 'update'])->name('supplier.update');
 Route::delete('/supplier/delete/{id}', [SupplierController::class, 'destroy'])->name('supplier.delete');
 
+
 // Customer Route
 Route::get('/customer', [CustomerController::class, 'index'])->name('customer.index');
 Route::get('/customer/create', [CustomerController::class, 'create'])->name('customer.create');
@@ -99,6 +105,7 @@ Route::put('/customer/update/{id}', [CustomerController::class, 'update'])->name
 Route::delete('/customer/delete/{id}', [CustomerController::class, 'destroy'])->name('customer.delete');
 
 
+//BARCODE
 Route::get('/barcode/{code}', function ($code) {
     return Cache::remember("barcode_{$code}", now()->addHours(6), function () use ($code) {
         $barcode = new DNS1D();
@@ -107,6 +114,8 @@ Route::get('/barcode/{code}', function ($code) {
     });
 })->name('barcode.show');
 
+
+//GET PRODUCT STOCK
 Route::get('/stock/get-qty/{product_id}', function ($id) {
     $stock = \App\Models\Stock::where('product_id', $id)->first();
     return response()->json(['qty' => $stock->qty_on_hand ?? 0]);
