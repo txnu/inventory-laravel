21f5656d9b0d0847eecddd65e3115ae6c1a18088
TANU WIJAYA <92568646+txnu@users.noreply.github.com>
2026-04-25 18:38:41 +0700

    add middleware

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
+
+class Authenticate extends Middleware {}
